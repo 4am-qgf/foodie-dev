5562ea04d48f232232f98ddd7de127abd1c28a93
song <1959077155@qq.com>
2021-09-16 01:59:17 +0800

    v1.1.1_20210916 验证用户是否存在

--- foodie-dev-api/src/test/java/com/test/TransTest.java
+++ foodie-dev-api/src/test/java/com/test/TransTest.java
@@ -1,17 +1,12 @@
 package com.test;
 
-import com.imooc.Application;
 import com.imooc.service.StuService;
 import com.imooc.service.TestTransService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class)
 public class TransTest {
 
     @Autowired
@@ -19,7 +14,7 @@ public class TransTest {
 
     @Autowired
     private TestTransService testTransService;
-    @Test
+    //@Test
     public void myTest() {
 //        stuService.testPropagationTrans();
         testTransService.testPropagationTrans();
