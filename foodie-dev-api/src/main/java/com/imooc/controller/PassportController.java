ddd2da138e7433ed0a0aa64c8270ccb21704ec90
4am-qgf <song18855132419>
2021-09-16 17:41:28 +0800

    v1.1.3_20210916 注册功能完成，swagger2完成

--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,14 +1,15 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Api(value = "注册登录" , tags = {"用于注册登录的相关接口"})
 @RestController
 @RequestMapping("passport")
 public class PassportController {
@@ -16,7 +17,7 @@ public class PassportController {
     @Autowired
     private UserService userService;
 
-
+    @ApiOperation(value = "用户名是否存在" , notes = "用户名是否存在" , httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
 
@@ -35,6 +36,33 @@ public class PassportController {
         return IMOOCJSONResult.ok();
     }
 
+    @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
+    @PostMapping("/regist")
+    public IMOOCJSONResult regist(@RequestBody UserBO userBO){
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+        String confirmPwd = userBO.getConfirmPassword();
 
+        //1判断用户名密码是否为空
+        if(StringUtils.isBlank(username)|| StringUtils.isBlank(password) || StringUtils.isBlank(confirmPwd)){
+            return IMOOCJSONResult.errorMsg("用户名或者密码不能为空");
+        }
+        //2判断用户名是否存在
+        boolean isExist = userService.queryUsernameIsExist(username);
+        if (isExist) {
+            return IMOOCJSONResult.errorMsg("用户名已经存在");
+        }
+        //3 密码不能低于6位
+        if (password.length() < 6) {
+            return IMOOCJSONResult.errorMsg("密码长度不能少于6");
+        }
+        //4两次密码是否一致
+        if (!password.equals(confirmPwd)) {
+            return IMOOCJSONResult.errorMsg("两次密码不一致");
+        }
+        userService.createUser(userBO);
+
+        return IMOOCJSONResult.ok();
+    }
 
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -2,12 +2,19 @@ package com.imooc.service.impl;
 
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
+import imooc.enums.Sex;
+import imooc.utils.DateUtil;
+import imooc.utils.MD5Utils;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
+
+import java.util.Date;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -15,7 +22,10 @@ public class UserServiceImpl implements UserService {
     @Autowired
     public UsersMapper usersMapper;
 
+    @Autowired
+    private Sid sid;
 
+    private static final String USER_FACE = "http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_8_qAIlFXAAAcIhVPdSg994.png";
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -27,8 +37,36 @@ public class UserServiceImpl implements UserService {
         userCriteria.andEqualTo("username", username);
 
         Users result = usersMapper.selectOneByExample(userExample);
-
         return result == null ? false : true;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users createUser(UserBO userBO) {
+        String userId = sid.nextShort();
+
+        Users user = new Users();
+        user.setId(userId);
+        user.setUsername(userBO.getUsername());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //默认用户昵称同用户名
+        user.setNickname(userBO.getUsername());
+        //默认头像
+        user.setFace(USER_FACE);
+        //默认生日
+        user.setBirthday(DateUtil.stringToDate("1998-01-01"));
+        //默认性别为保密
+        user.setSex(Sex.secret.type);
+
+        user.setCreatedTime(new Date());
+
+        user.setUpdatedTime(new Date());
+        usersMapper.insert(user);
+        return user;
     }
 
 
